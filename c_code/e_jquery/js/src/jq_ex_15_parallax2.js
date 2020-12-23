69fad353aa6d26cbca74ead5a1abb8f329c201db
mingv4 <mingv4@gmail.com>
2020-12-23 15:17:59 +0900

    pinterest up

---
+++ c_code/e_jquery/js/src/jq_ex_15_parallax2.js
@@ -0,0 +1,67 @@
+// ex_15_parallax2.js
+
+(function($){
+    var win = $(window);
+    var winH = win.outerHeight();
+  
+    var headBox = $('#headBox');
+    var h1 = headBox.find('h1');
+    var frontImg = headBox.find('.front_image');
+    var backImg = headBox.find('.back_image');
+  
+    var setN = 1.5;
+  
+  
+    // 브라우저 스크롤시 수행
+    win.on('scroll', function(){
+      var winSt = $(this).scrollTop();
+      var per = winSt / winH;
+      var scaleR = 1+per;
+      var opacityR, airPer, logoPer;
+  
+    // 4. logo 사라지게 만들기 
+    // 5. logo사라지는 동안 앞에 투명막 생기게하기
+    var logoR = 0.5;
+     if(per >= logoR){
+      logoPer = (1 + logoR) - per;
+       console.log( logoR + per );    
+      h1.css({opacity: logoPer, transform:'scale('+ (logoR + per) + ')' });
+  
+      headBox.addClass('default');
+  
+     }else{
+       headBox.removeClass('default');
+     }
+  
+    // 1. frontImg가 점점 커지게( transform:scale() );
+    // 2. 점점 사라지게
+    opacityR = setN + 1 - per; 
+  
+      if(scaleR <= setN){
+        // console.log('scale: ', 1 + per);
+        frontImg.css({transform:'scale(' + scaleR + ')'});
+      }else if(scaleR >= setN + 1){      
+        // console.log('opacity: ', opacityR+1);         
+        frontImg.css({opacity: opacityR});
+      }
+  
+    // 6. frontImg 의 투명도가 0이되면 headBox를 사라지게 만들기
+    if(opacityR < 0){
+      // console.log('사라진위치: ', win.scrollTop() );
+      headBox.hide();
+    }else{
+      headBox.show();
+    }
+  
+    // 3. 비행기 나타나면서 올라가기
+      if(scaleR > setN){
+        airPer = (scaleR - setN) * 100;
+        // console.log( airPer );
+        backImg.css({backgroundPositionY: airPer + '%'})
+      }
+  
+    }); // win.on('scroll')
+  
+  
+  
+  })(jQuery);
---
+++ c_code/e_jquery/js/src/jq_ex_16_like_pinterest.js
@@ -0,0 +1,109 @@
+//jq_ex_16_like_pinterest.js
+
+(function($){
+    /** todo list...
+     * [o] 카드 js 에서구현하기 
+     * [x] json파일 생성해서 불러오기/이미지 리스트화 처리
+     * [] 생성된 ul의 가로 크기를 li의 갯수만큼 담을 수 있도록 수정
+     */
+
+     /* 실제 데이터 형식 에서는
+     [
+         { "title" : "카드 제목", "content" : "카드 내용",
+         "imgSrc" : 'random_01.jpg', "imgAlt" : '이미지 설명'},
+         { "title" : "카드 제목", "content" : "카드 내용",
+         "imgSrc" : 'random_02.jpg', "imgAlt" : '이미지 설명'},
+         { "title" : "카드 제목", "content" : "카드 내용",
+         "imgSrc" : 'random_03.jpg', "imgAlt" : '이미지 설명'}
+     ];
+     */
+
+    var cardData = [];
+    var k = 0, l;
+    var url="../img/sample_02/";
+    var imgT = 'random_';
+    for ( ; k < 19; k++){
+        if(k < 9){
+          l='0'+(k+1);  // 8 ->009 
+        }else if (k < 99){ // ->010
+          l = (k+1);
+        }
+        
+        cardData[k] = {};
+
+         cardData[k].title = 'title' + l;
+         cardData[k].imgSrc = url + imgT + l + '.jpg';
+         cardData[k].imgAlt = imgT + l + '이미지설명';
+        // cardData[k] = imgT + l + '.jpg';
+      }
+      // console.log(cardData);
+
+
+
+  var win = $(window);
+  var cardBox = $('#cardBox');
+
+  cardBox.append('<ul></ul>');
+  var cardUl = cardBox.children('ul');
+  var liText = '<li><a><div class="img_temp"><img></div><span></span></a></li>';
+  var y = 0;
+  var listEl;
+  for(; y < 19; y += 1){
+    cardUl.append(liText);
+    listEl = cardUl.children('li').eq(y);
+    listEl.find('img').attr ({'src':cardData[y].imgSrc,
+                              'alt':cardData[y].imgAlt });
+    listEl.find('span').text(cardData[y].title);
+    listEl.find('a').attr({"href":'#'});
+  }
+  var cardLi = cardUl.children('li');
+ 
+
+  // ul의 크기설정
+  // 1. li의 가로크기, li의 마진값(공백)
+  // 2. 브라우저에서의 기준 크기
+  
+  var cardLiWidth = cardLi.eq(0).outerWidth();
+  var cardMargin = cardLi.eq(0).outerWidth(true) - cardLiWidth;
+  // outerWidth(true) -> 마진값을 포함한 가로값
+  // outerWidth() -> 마진값을 제외한 가로값(padding+border+width)
+
+  /*  여기서부터 브라우저크기가 변경되면 다시 수행!!!!!! - 함수처리 */
+  var BrowserSet = function(){
+    // 함수가 재수행 하면 li style속성 제거
+    cardLi.removeAttr('style'); 
+
+    var winW = win.outerWidth();
+    var liLen = parseInt( winW / (cardLiWidth + cardMargin) );
+    // 정수화처리 : parseInt() 
+    // 버림,올림,반올림:  Math.floor(), Math.ceil(), Math.round()
+    // console.log( liLen );
+    var ulWidth = ((cardLiWidth + cardMargin) * liLen) - cardMargin;
+    cardUl.css({width: ulWidth + 'px'});
+    // console.log(liLen, ulWidth);
+    // li설정
+    // 1. li의 순번에서 가로 행에들어가는 마지막번째에는 margin 제거
+    // cardLi.eq(liLen-1).css({margin:0});
+    // 2. 각각의 행에 들어가는 모든 마지막위치의 갯수번째 margin 제거
+    var lin = 0;
+    var liNth = Math.ceil(cardLi.length / liLen);
+    var n;
+    for(; lin < liNth; lin += 1){
+      n = (liLen * lin) - 1;
+      // console.log(n);
+      cardLi.eq(n).css({marginRight:0});
+    };
+
+    // 가로에 4개씩에서 4번째( eq(4*n-1) )마다 마진값 x  -> 3,7,11,15...
+    // 가로에 6개씩에서 6번째( eq(6*n-1) )마다 마진값 x  -> 5,11,17,23...
+    // 브라우저크기가 변경될때마다 재측정 
+}; // BrowserSet();
+BrowserSet();
+
+win.on('resize', function(){
+  BrowserSet();
+});
+
+
+
+})(jQuery);
