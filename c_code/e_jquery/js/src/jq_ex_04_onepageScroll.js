5c0e30fa742be3fef23d8e700742a71b3420ac07
mingv4 <mingv4@gmail.com>
2020-12-29 18:01:18 +0900

    code up

--- c_code/e_jquery/js/src/jq_ex_04_onepageScroll.js
+++ c_code/e_jquery/js/src/jq_ex_04_onepageScroll.js
@@ -1,188 +1,118 @@
 // jq_ex_04_onepageScroll.js
 
 (function($){
-<<<<<<< HEAD
-    // jQuery Start
-  /*
-     1. 스크롤시 $('#headBox') 상단 고정
-     1-1. 상단이동버튼 기능 추가
-     2. $('popup_dp') 의 닫기 버튼 클릭 시 팝업창 삭제 
-  */
+    // jQuery start
+    /**
+     * 1. 스크롤시 $('#headBox') 상단 고정
+     * 1-1. 상단이동버튼 기능추가
+     * 2. $('.popup_dp')의 닫기버튼 클릭시 팝업창 삭제
+     */
   
-  /* 1번 사용 시 : offset().top, scrollTop, on('scroll'), $(window)
-     
-  */
-  var win = $(window);       //window라고 변수를 지정하는것은 오류.
-  var headBox = $('#headBox');
-  var navBox = $('#navBox');
-  var navLi = navBox.find('li');
-  var topBtn = $('.top_move_btn');
-  // 브라우저 상단에서 떨어져 있는 정도 체크
+     /** 1번사용시 : offset().top,  scrollTop, on('scroll'), $(window) */
+      var win = $(window);
+      var headBox = $('#headBox');
+      var nav     = $('#navBox');
+      var navBtn   = nav.find('li');
+      var topBtn = $('.top_move_btn');
   
-  var headOffset = headBox.offset().top;
   
-  // console.log(headOffset);
-  // 1-1번 보이지 않았다가, 1000px 이동 후에 나타나기
-  topBtn.hide();
+      // 브라우저 상단에서 떨어져 있는 양 체크
+      var headOffset = headBox.offset().top;
+      console.log('offset: ', headOffset );
   
-  win.on('scroll', function(e){
-      // 스크롤이 이동한 위치 값 파악
-      var winSt = win.scrollTop();
-      // console.log(winSt);
+      // 1-1번 보이지 않았다가, 1000px이동후에 나타나기
+      topBtn.hide();
   
-      if(winSt >= headOffset){    //headOffset - 스크롤이 떨어져있는 정도보다 winSt - 스크롤이 이동한 위치값이 크다면 상단에 고정시켜라.
-          headBox.css({'position':'fixed', 'top':0, 'zIndex':1500});
-      } else {
-          headBox.removeAttr('style');
-      }
+      win.on('scroll', function(e){
+        // 스크롤이 이동한 위치값 파악
+        var winSt = win.scrollTop();
+        console.log(winSt);
   
-      if(winSt >= 1000){  // (winSt >= 500) ?  ---- 조건
-          topBtn.fadeIn(); // topBtn.stop().fadeIn()  ---- 참
-      } else {
-          topBtn.stop().fadeOut(); //: topBtn.stop().fadeOut(); ---- 거짓
-      }
+        // if( winSt >= headOffset ) {
+        //   headBox.css({'position':'fixed', 'top':0, 'zIndex':1500});
+        // }else{
+        //   headBox.removeAttr('style');
+        // }
   
-      // (winSt >= 500) ? topBtn.stop().fadeIn() : topBtn.stop().fadeOut(); ------ 위의 조건문과 같음.
-  });
-  //-------------------------------------------------------------------------------
-  // 클릭 시 처리되는 내용을 별도의 함수로 처리
+        (winSt >= headOffset) ?  
+          headBox.css({'position':'fixed', 'top':0, 'zIndex':1500}) : 
+          headBox.removeAttr('style') ;
+  
+        // if( winSt >= 500){
+        //   topBtn.stop().fadeIn();
+        // }else{
+        //   topBtn.stop().fadeOut();
+        // }
+  
+        (winSt >= 500) ? topBtn.stop().fadeIn() : topBtn.stop().fadeOut();
+      }); // win.on('scroll')
+  // -------------------------------------------------------------------
+  // 클릭시 처리되는 내용을 별도의 함수로 처리
   var liScrollMove = function(e){
-      e.preventDefault();
-      var it = $(this).find('a');
-      var itAttr = it.attr('href');  // a요소의 연결된 선택자 파악
-      var itOffset = $(itAttr).offset().top;  // a요소의 href 값이 상단에서 떨어져 있는 정도 체크
-      $('html, body').animate({ scrollTop: itOffset });  // 브라우저를 이동시켜라(스크롤) -> itOffset의 파악된 크기만큼
-  }; //liScrollMove();
-  //-------------------------------------------------------------------------------
-  // topBtn.on('click',['a'], liScrollMove);
-  // navLi.on('click',['a'], liScrollMove);
-  //    선택자(). forEach(function(배역요소각각, 해당 배열요소 순서){});  ------ js
-  //    $.each(배열, function(배열요소 순서, 순서에 맞는 배열요소 각각){});
+    e.preventDefault();
+    var it = $(this).find('a');
+    // a요소의 연결된 선택자 파악
+    var itAttr = it.attr('href'); 
+    // a요소의 href값이 상단에서 떨어져있는 양 체크
+    var itOffset = $(itAttr).offset().top; 
+    // 브라우저를 이동시켜라(스크롤) -> itOffset의로 파악된 크기만큼
+    $('html, body').animate({ scrollTop: itOffset });
+  }; // liScrollMove();
+  // -------------------------------------------------------------------
+      // topBtn.on('click', ['a'], liScrollMove);
+      // navBtn.on('click', ['a'], liScrollMove);
+      // 배열.forEach(function(배열요소각각, 해당배열요소순서){});
+      // $.each(배열, function(배열요소순서, 순서에맞는배열요소각각){});
+      
+      // 동일한기능을 처리, 하나의함수를 수행함으로인하여, 기능을 하나로 묶어주기
+      var btnCollection = [topBtn, navBtn];
+      $.each(btnCollection, function(i, btn){
+        btn.on('click', ['a'], liScrollMove);
+      });
   
-  var btnCollection = [topBtn,navLi]  // 위의 topBtn, navLi 선택자를 하나로 합침
-  $.each(btnCollection,function(i,btn){
-      btn.on('click',['a'],liScrollMove);
-  });
-  //-------------------------------------------------------------------------------
-  //2번 기능
-  
+  // ------------------------------------------------
+  // 2번기능
   var popupDp = $('.popup_dp');
   var popupBtn = popupDp.find('button');
-  popupBtn.on('click',function(e){
-      e.preventDefault();
-      popupDp.remove();
+  popupBtn.on('click', function(e){
+    e.preventDefault();
+    popupDp.remove();
   });
   
-  //--------------------------------------------------------------------------------
-  
-  // 3. tab 메뉴
-  
+  // -------------------------------------------------
+  // 3. tab메뉴 
   var tab = $('.tab_li');
   var tabLi = tab.find('li');
   var tabConWrap = $('.tab_content');
   var tabCon = tabConWrap.find('div');
   
-  tabLi.on ('click',['a'],function(e){
-      e.preventDefault();
-      var it = $(this);
-      var itI = it.index();
   
-      tabCon.eq(itI).show();
-      tabCon.eq(itI).siblings().hide();
-  });
-  
-    //jQuery End
-  })(jQuery);
-=======
-  // jQuery Start
-/*
-   1. 스크롤시 $('#headBox') 상단 고정
-   1-1. 상단이동버튼 기능 추가
-   2. $('popup_dp') 의 닫기 버튼 클릭 시 팝업창 삭제 
-*/
-
-/* 1번 사용 시 : offset().top, scrollTop, on('scroll'), $(window)
-   
-*/
-var win = $(window);       //window라고 변수를 지정하는것은 오류.
-var headBox = $('#headBox');
-var navBox = $('#navBox');
-var navLi = navBox.find('li');
-var topBtn = $('.top_move_btn');
-// 브라우저 상단에서 떨어져 있는 정도 체크
-
-var headOffset = headBox.offset().top;
-
-// console.log(headOffset);
-// 1-1번 보이지 않았다가, 1000px 이동 후에 나타나기
-topBtn.hide();
-
-win.on('scroll', function(e){
-    // 스크롤이 이동한 위치 값 파악
-    var winSt = win.scrollTop();
-    // console.log(winSt);
-
-    if(winSt >= headOffset){    //headOffset - 스크롤이 떨어져있는 정도보다 winSt - 스크롤이 이동한 위치값이 크다면 상단에 고정시켜라.
-        headBox.css({'position':'fixed', 'top':0, 'zIndex':1500});
-    } else {
-        headBox.removeAttr('style');
-    }
-
-    if(winSt >= 1000){  // (winSt >= 500) ?  ---- 조건
-        topBtn.fadeIn(); // topBtn.stop().fadeIn()  ---- 참
-    } else {
-        topBtn.stop().fadeOut(); //: topBtn.stop().fadeOut(); ---- 거짓
-    }
-
-    // (winSt >= 500) ? topBtn.stop().fadeIn() : topBtn.stop().fadeOut(); ------ 위의 조건문과 같음.
-});
-//-------------------------------------------------------------------------------
-// 클릭 시 처리되는 내용을 별도의 함수로 처리
-var liScrollMove = function(e){
-    e.preventDefault();
-    var it = $(this).find('a');
-    var itAttr = it.attr('href');  // a요소의 연결된 선택자 파악
-    var itOffset = $(itAttr).offset().top;  // a요소의 href 값이 상단에서 떨어져 있는 정도 체크
-    $('html, body').animate({ scrollTop: itOffset });  // 브라우저를 이동시켜라(스크롤) -> itOffset의 파악된 크기만큼
-}; //liScrollMove();
-//-------------------------------------------------------------------------------
-// topBtn.on('click',['a'], liScrollMove);
-// navLi.on('click',['a'], liScrollMove);
-//    선택자(). forEach(function(배역요소각각, 해당 배열요소 순서){});  ------ js
-//    $.each(배열, function(배열요소 순서, 순서에 맞는 배열요소 각각){});
-
-var btnCollection = [topBtn,navLi]  // 위의 topBtn, navLi 선택자를 하나로 합침
-$.each(btnCollection,function(i,btn){
-    btn.on('click',['a'],liScrollMove);
-});
-//-------------------------------------------------------------------------------
-//2번 기능
-
-var popupDp = $('.popup_dp');
-var popupBtn = popupDp.find('button');
-popupBtn.on('click',function(e){
-    e.preventDefault();
-    popupDp.remove();
-});
-
-//--------------------------------------------------------------------------------
-
-// 3. tab 메뉴
-
-var tab = $('.tab_li');
-var tabLi = tab.find('li');
-var tabConWrap = $('.tab_content');
-var tabCon = tabConWrap.find('div');
-
-tabLi.on ('click',['a'],function(e){
+  tabLi.on('click',['a'], function(e){
     e.preventDefault();
     var it = $(this);
     var itI = it.index();
-
+  
     tabCon.eq(itI).show();
     tabCon.eq(itI).siblings().hide();
-});
-
-  //jQuery End
-})(jQuery);
->>>>>>> 8f14b0ec6f85631a5772223400410bee3eb470ef
+  });
+  // --------------------------------------------------
+  var titleTab = $('.title_tab_style');
+  var titleTabDl = titleTab.children('dl');
+  var titleTabDt = titleTabDl.children('dt');
+  var titleTabDd = titleTabDl.children('dd');
+  
+  titleTabDt.eq(0).addClass('action');
+  
+  titleTabDt.on('click', ['a'],function(e){
+    e.preventDefault();
+    var it = $(this);
+    
+    it.siblings('dt').removeClass('action');
+    it.addClass('action');
+  
+    titleTabDd.hide();
+    it.next('dd').show();
+  });
+  // --------------------------------------------------
+    // jQuery end
+  })(jQuery);
---
+++ c_code/e_jquery/js/src/jq_ex_18_like_pinterest2.js
@@ -0,0 +1,172 @@
+// ex_18_like_pinterest2.js
+
+(function($){
+  // 1. 카드 감싸는영역 크기설정, 내용물 삽입 
+  // 2. 스크롤의 진행위치값 표시
+  // 3. 스크롤시 일정위치가 넘어가면 추가 카드가 생성되게 처리
+  // 4. Math.random()을 이용하여 색상, 높이값을 변경
+  // 5. 임시로 세팅했던 .card의 float기능은 삭제, position처리
+  // 6. 화면상의 하나의 행에 들어갈 위치값(left값) 설정
+  // 7. 화면상의 열에 해당하는 위치값(top) 설정
+  // 8. .card_area의 높이값을 강제 설정
+
+
+  
+  // 4-1. 컬러 랜덤 색상설정
+  var Rn = function(){
+    return  Math.floor(Math.random()*16).toString(16);
+  };
+  // var randomColor = '#'+Rn()+Rn()+Rn();  
+  // console.log(randomColor);
+
+  // 4-2. 높이값 랜덤수치 변경
+  var myRandomN = 7;
+  var myRandomH = 40;
+  var RandomHeight = function(){
+    var h = parseInt(Math.random() * myRandomN) * myRandomH;
+    return h;
+  };
+
+// ================================
+// 2번기능 스크롤시 진행위치값
+  var headBox = $('#headBox');
+  headBox.prepend('<div class="scroll_percent"></div>');
+  var scrollP = $('.scroll_percent ');
+  scrollP.css({
+    position:'absolute', top:0, left:0,zIndex:-1,
+    width:0, height:'90%', opacity:0.2,
+    backgroundColor:'#f06',
+    transform:'translate(-0.5rem) scale(1.1) skew(-15deg)'
+  });
+// 2번기능 세팅 ----------------------------------
+// ================================
+  var cardCode = '<div class="card"><div class="card_img"></div><div class="card_narr"><a href="#"><dl><dt>title</dt><dd>Lorem, ipsum.</dd></dl></a></div></div>';
+
+  // window, cardBox, card_area 선택자
+  var win = $(window);
+  var cardBox = $('#cardBox');
+  var cardArea = cardBox.children('.card_area');
+  
+  // 추가 선택자 및 변수
+  var card; 
+  var cardWidthLen;
+
+  // 1. card 생성
+  // 3번에서 요구하는 카드생성형태이므로 중복 수행이기에 함수화 처리
+  var i=0;   
+  // 카드 전체 값
+  var cardCount = 0;
+  var randomColor = [];
+  var cardRandomHeight = []; 
+
+  var AppendCardFn = function(){
+    cardCount += 50; 
+    // var r1,r2,r3;
+    
+    for(; i < cardCount; i++){
+      // r1 = parseInt(Math.random()*16).toString(16);
+      // r2 = parseInt(Math.random()*16).toString(16);
+      // r3 = parseInt(Math.random()*16).toString(16);
+      cardArea.append(cardCode);
+      card = cardArea.children('.card').eq(i);
+      card.find('dt').text( 'title_'+ (i+1) );
+      randomColor[i] = '#'+Rn()+Rn()+Rn();
+      // randomColor[i] = '#'+r1+r2+r3;
+      card.find('.card_img').css({
+        backgroundColor:randomColor[i]
+      });
+      // card높이값을 기존값과 random함수값을 합쳐서 처리
+      cardRandomHeight[i]=card.outerHeight()+RandomHeight();
+      card.css({height:cardRandomHeight[i] + 'px'});
+    }
+    // console.log( randomColor );
+
+    // 5.삭제
+    // $('.card').css({float:'left'}); 
+    card = $('.card');
+  }// AppendCardFn();
+  AppendCardFn();
+  // =======================================
+  // =======================================
+  // .card_area의 가로값 세팅
+  var cardEq0 = $('.card').eq(0);
+  var cardWidth = cardEq0.outerWidth(); 
+  //outerWidth = 가로+패딩+외곽선;
+  var CardWidthSet = function(){
+    cardWidthLen = 
+    parseInt(cardBox.outerWidth() / cardWidth);
+    cardArea.css({width:cardWidth * cardWidthLen + 'px'});     
+    return cardWidthLen;   
+  }// CardWidthSet();
+  CardWidthSet();
+  // =======================================
+  // 6,7. 카드의 위치값 설정
+  var remainder;
+  // card = $('.card');
+  var topArr = [];
+
+  var CardPositionSettingFn = function(){
+    var j=0;
+    for(; j < cardCount ; j+=1){
+      // 가로행에들어가는 각각 n번째(나머지값에 해당하는 번째)
+      //6. 요소의 위치를(left값) 배치      
+     remainder = j % cardWidthLen;
+     card.eq(j).css({left: cardWidth*remainder+'px'});
+     var setTop, setHeight;
+     // 7. 요소의 추가 위치(top값) 배치
+      if(j < cardWidthLen){
+        card.eq(j).css({top:0});
+        topArr[j] = 0;
+      }else{
+        setTop = parseFloat(card.eq(j-cardWidthLen).css('top'));
+        setHeight = card.eq(j-cardWidthLen).outerHeight();
+        topArr[j] = setTop + setHeight;
+        card.eq(j).css({top: setTop + setHeight +'px'});
+      }
+    } // for()
+    // 8. .card_area의 높이값 설정
+    // 각각의 카드 높이값(height) 중의 가장큰값
+    var randomHMax = card.eq(0).outerHeight() + (myRandomN * myRandomH);
+    // 카드의 position의 위치 top의 최대값
+    var maxN = Math.max.apply(null, topArr);
+    // console.log(randomHMax, maxN)
+    cardArea.css({height:randomHMax + maxN +'px'});
+  }// CardPositionSettingFn();
+  // =======================================
+  // 함수기능수행 
+  CardPositionSettingFn();
+  
+  // =======================================
+  // 브라우저 크기 변경시 cardArea 사이즈 재수정
+  win.on('resize', function(){
+    CardWidthSet();
+    CardPositionSettingFn();
+  });
+
+// -----------------------------------
+// 2번기능 스크롤시처리 
+  win.on('scroll', function(){
+    var st = $(this).scrollTop();
+    var wrap = $('#wrap');
+    var wrapHeight = wrap.outerHeight();
+    var wihH = win.outerHeight();
+
+    // 퍼센트 공식: 값 / 기준(전체) * 100
+    // 스크롤처리시에는 화면의 높이값 만큼 빼주어야한다.
+    var per = st / (wrapHeight-wihH) * 100;
+    // console.log(per);
+    // .scroll_percent 의 가로값에 적용
+    scrollP.css({width: per + '%'});
+
+    // 3번기능 일정수치가 넘어가면 추가 카드를 생성
+    // 카드를 생성한 영역을 재호출, 기존의 카드는 유지
+    if(per >= 100){
+      AppendCardFn();
+      CardPositionSettingFn();
+    }
+  });
+// -----------------------------------
+
+
+
+})(jQuery);
