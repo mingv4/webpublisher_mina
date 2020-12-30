f91a90673eb20d95d9856c586d195ae7ecd5ff9b
mingv4 <mingv4@gmail.com>
2020-12-30 18:00:34 +0900

    upup

--- lotteworld_aquarium/js/src/lw_pl_viewBox_landing.js
+++ lotteworld_aquarium/js/src/lw_pl_viewBox_landing.js
@@ -8,16 +8,23 @@
   
     var viewBoxOffset = viewBox.offset().top;
     var conScroll;
+    var getScroll;
+    var scrollImageP = function(){
+      getScroll = win.scrollTop(); // 스크롤 값
+      conScroll = getScroll - (viewBoxOffset - winH);
+      viewBox.css({'backgroundPositionY':-conScroll/3 + 'px' });
+    };
+    scrollImageP();
+
     win.on('scroll', function(e){
-        var getScroll = win.scrollTop(); // 스크롤 값
+       
         // console.log( getScroll );
     
         // 스크롤값이, 
         // conBox의 offset().top (에서 브라우저 높이값 일부빠진만큼)보다 크면 수행
         if(getScroll >= viewBoxOffset - winH){
             viewBox.addClass('action');
-          conScroll = getScroll - (viewBoxOffset - winH);
-          viewBox.css({'backgroundPositionY':-conScroll/3 + 'px' });
+            scrollImageP();
               }else{
                 viewBox.removeClass('action');
         }
